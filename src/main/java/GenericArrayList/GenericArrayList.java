5e2a0127c4d898bc43212902f4010e7bc1df3f74
Eric O'Brien <obrienjeric@gmail.com>
2022-02-13 15:21:01 -0500

    Initial commit

---
+++ src/main/java/GenericArrayList/GenericArrayList.java
@@ -0,0 +1,140 @@
+package GenericArrayList;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class GenericArrayList<T> {
+
+    private T[] genericArray;
+    private Integer length;
+
+    public GenericArrayList() {
+    }
+
+    public GenericArrayList(T[] genericArray) {
+        this.genericArray = genericArray;
+
+    }
+
+    public T[] getGenericArray() {
+        return genericArray;
+    }
+
+    public void setGenericArray(T[] genericArray) {
+        this.genericArray = genericArray;
+    }
+
+    public Integer getLength() {
+        return this.genericArray.length;
+    }
+
+
+    @Override
+    public String toString() {
+        return "GenericArrayList{" +
+                "genericArray=" + Arrays.toString(genericArray) +
+                ", length=" + genericArray.length +
+                '}';
+    }
+
+    public T getElementAtIndex(int index) {
+
+        return this.genericArray[index];
+    }
+
+    public Integer getIndexOfFirstNull(T[] genericArray) {
+
+        Integer counter = 0;
+
+        for(int i = 0; i < genericArray.length; i++) {
+            if(genericArray[i] != null) {
+                counter ++;
+            } else {
+                return counter;
+            }
+        }
+
+        return counter;
+    }
+
+    public T[] addElement(T element)  {
+
+        T[] genericArray = this.genericArray;
+        InputMismatchException inputMismatchException = new InputMismatchException("Data type must be: " + genericArray[0].getClass());
+        Integer currentLength = this.getIndexOfFirstNull(genericArray);
+
+        if(element.getClass() != genericArray[0].getClass()) {
+            throw inputMismatchException;
+        }
+            if (currentLength >= genericArray.length) {
+                T[] newGenericArray = (T[]) new Object[currentLength * 2];
+
+                for (int i = 0; i < genericArray.length; i++) {
+                    newGenericArray[i] = genericArray[i];
+                }
+                for (int j = 0; j < newGenericArray.length; j++) {
+                    if (newGenericArray[j] == null) {
+                        currentLength = j;
+                        newGenericArray[j] = element;
+                        break;
+                    }
+                }
+                return newGenericArray;
+            } else {
+                genericArray[currentLength] = element;
+            }
+
+            return genericArray;
+    }
+
+    public T[] expandArray(T[] genericArray) {
+
+        Integer length = genericArray.length;
+
+        GenericArrayList genericArrayList = new GenericArrayList(genericArray);
+        Integer number = genericArrayList.getIndexOfFirstNull(genericArray);
+
+        Object[] newGenericArray = new Object[length * 2];
+        System.out.println(newGenericArray.length);
+
+        for(int i = 0; i < length; i++) {
+            newGenericArray[i] = genericArray[i];
+        }
+        genericArray = (T[]) newGenericArray;
+
+
+        return genericArray;
+
+    }
+
+    public void addToArray(T element, T[] genericArray) {
+
+        Integer length = genericArray.length;
+        GenericArrayList genericArrayList = new GenericArrayList(genericArray);
+        IndexOutOfBoundsException indexOutOfBoundsException = new IndexOutOfBoundsException("Array must be expanded before accepting additional elements");
+        Integer indexOfFirstNull = this.getIndexOfFirstNull(genericArray);
+
+        if (indexOfFirstNull == length) {
+            throw indexOutOfBoundsException;
+        } else {
+            Object[] newArray = genericArrayList.addElement(element);
+            genericArray = (T[]) newArray;
+
+        }
+
+
+
+
+
+    }
+
+    //    public static <T> void genericToString(T[] elements) {
+//        for(T element : elements) {
+//            System.out.printf("%s ", element);
+//            System.out.println();
+//        }
+//    }
+
+
+}
---
+++ src/main/java/GenericArrayList/Main.java
@@ -0,0 +1,65 @@
+package GenericArrayList;
+
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Integer[] intArr = {102, 108, 333};
+        Double[] doubArr = {22.22, 33.4, 56.2};
+        Character[] chArr = {'X', 'T', 'A'};
+
+        System.out.println("Course codes:");
+
+
+
+
+        GenericArrayList genericArrayList = new GenericArrayList(chArr);
+
+        for (Object object : genericArrayList.getGenericArray()) {
+            System.out.println(object);
+        }
+
+        System.out.println(genericArrayList.toString());
+
+        System.out.println(genericArrayList.getElementAtIndex(1));
+
+        Object[] newChArr = genericArrayList.addElement('B');
+
+        GenericArrayList genericArrayList1 = new GenericArrayList(newChArr);
+
+        Object[] newChArr2 = genericArrayList1.addElement('8');
+
+        System.out.println(Arrays.toString(newChArr2));
+
+        GenericArrayList genericArrayList2 = new GenericArrayList(newChArr2);
+
+        Object[] newChArr3 = genericArrayList2.addElement('T');
+
+        System.out.println(Arrays.toString(newChArr3));
+
+        GenericArrayList genericArrayList3 = new GenericArrayList(newChArr3);
+
+        Object[] newChArr4 = genericArrayList3.addElement('S');
+
+        GenericArrayList genericArrayList4 = new GenericArrayList(newChArr4);
+
+        genericArrayList4.addToArray('F', newChArr4);
+        genericArrayList.addToArray('U', newChArr4);
+        genericArrayList.addToArray('P', newChArr4);
+        genericArrayList.addToArray('O', newChArr4);
+        genericArrayList.addToArray('5', newChArr4);
+
+        Object[] newChArr5 = genericArrayList.expandArray(newChArr4);
+
+        genericArrayList.addToArray('4', newChArr5);
+        genericArrayList.addToArray('$', newChArr5);
+        genericArrayList.addToArray('I', newChArr5);
+        genericArrayList.addToArray('C', newChArr5);
+
+
+        System.out.println("chArr5: " + Arrays.toString(newChArr5));
+    } //end of main method
+
+}
